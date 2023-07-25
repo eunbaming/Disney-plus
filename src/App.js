eb8edee0866aecefa9e4cf10456d4925f0798ef4
eunbaming <dbskqnsghd@naver.com>
2023-07-25 09:56:18 +0900

    feat: Creating a video banner using Styled Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import styled from "styled-components";
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &::after {
+    position: absolute;
+    content: "";
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    inset: 0;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 function App() {
   return (
-    <div>
+    <Container>
       <Nav />
-    </div>
+    </Container>
   );
 }
 
