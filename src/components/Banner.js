cdd1debef896e1203f37c9638c4b571b77731278
eunbaming <dbskqnsghd@naver.com>
2023-08-02 14:01:29 +0900

    feat: Creating image banner and video banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,44 @@
 import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
 import request from "../api/request";
+import "./Banner.css";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
+  const [isClicked, setIsClicked] = useState(false);
 
   const fetchData = async () => {
     // 현재 상영중인 여러 영화 정보를 가져오기
@@ -22,9 +54,59 @@ const Banner = () => {
       params: { append_to_response: "videos" },
     });
     setMovie(movieDetail);
+    console.log("movieDetail", movieDetail);
   };
 
-  return <div>Banner</div>;
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  };
+
+  if (isClicked) {
+    return (
+      <>
+        <Container>
+          <HomeContainer>
+            <Iframe
+              src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+              width="640"
+              height="360"
+              allow="autoplay; fullscreen"
+            ></Iframe>
+          </HomeContainer>
+        </Container>
+        <button onClick={() => setIsClicked(false)}>X</button>
+      </>
+    );
+  } else {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner_contents">
+          <h1 className="banner_title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+          <div className="banner_buttons">
+            {movie.videos?.results[0]?.key && (
+              <button
+                className="banner_button play"
+                onClick={() => setIsClicked(true)}
+              >
+                Play
+              </button>
+            )}
+          </div>
+          <p className="banner_description">{truncate(movie.overview, 100)}</p>
+        </div>
+        <div className="banner_fadeBottom"></div>
+      </header>
+    );
+  }
 };
 
 export default Banner;
