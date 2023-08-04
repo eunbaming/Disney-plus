3260f16e109970f866565e81cebddff193fec155
eunbaming <dbskqnsghd@naver.com>
2023-08-04 16:40:03 +0900

    feat: Create a row component, bring in the relevant movies api and show them

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Banner from "./components/Banner";
 import Category from "./components/Category";
 import Nav from "./components/Nav";
 import styled from "styled-components";
+import Row from "./components/Row";
+import request from "./api/request";
 
 const Container = styled.main`
   position: relative;
@@ -29,6 +31,10 @@ function App() {
       <Nav />
       <Banner />
       <Category />
+      <Row title="Trending Now" id="TN" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={request.fetchComedyMovies} />
     </Container>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,43 @@
-import React from "react";
+import axios from "../api/axios";
+import React, { useCallback, useEffect, useState } from "react";
+import "./Row.css";
 
-const Row = () => {
-  return <div>Row</div>;
+const Row = ({ title, id, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviData = useCallback(async () => {
+    const response = await axios.get(fetchUrl);
+    console.log("res", response);
+    setMovies(response.data.results);
+  }, [fetchUrl]);
+
+  useEffect(() => {
+    fetchMoviData();
+  }, [fetchMoviData]);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider_arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+        <div id={id} className="row_posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className="row_poster"
+              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+              alt={movie.title}
+            />
+          ))}
+        </div>
+        <div className="slider_arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Row;
