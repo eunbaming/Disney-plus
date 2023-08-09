7b1a62dd55db44b2602280e1c9ed35106bd485db
eunbaming <dbskqnsghd@naver.com>
2023-08-09 16:22:29 +0900

    feat: Creating and styling the MovieModal UI

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -11,7 +11,31 @@ const MovieModal = ({
   vote_average,
   setOpenModal,
 }) => {
-  return <div>MovieModal</div>;
+  return (
+    <div className="presentation" role="presentation">
+      <div className="wrapper-modal">
+        <div className="modal">
+          <span className="modal_close" onClick={() => setOpenModal(false)}>
+            X
+          </span>
+          <img
+            className="modal_poster-img"
+            src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+            alt="modal-img"
+          />
+          <div className="modal_content">
+            <p className="modal_details">
+              <span className="modal_user-perc">100% for you</span>{" "}
+              {release_date ? release_date : first_air_date}
+            </p>
+            <h2 className="modal_title">{title ? title : name}</h2>
+            <p className="modal_overview">평점 : {vote_average}</p>
+            <p className="modal_overview">{overview}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieModal;
