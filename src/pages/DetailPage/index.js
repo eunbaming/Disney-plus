6ed9a427ec55a2ed51b643b5774164e6305736e3
eunbaming <dbskqnsghd@naver.com>
2023-08-23 15:49:05 +0900

    feat: Implementing movie detail page using useParams

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "../../api/axios";
 
 const DetailPage = () => {
-  return <div>DetailPage</div>;
+  // movieId : App.js detail-page 경로에 적어둔 이름
+  let { movieId } = useParams();
+  console.log("movieId", movieId);
+  let [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`/movie/${movieId}`);
+      console.log("detail res", response);
+      setMovie(response.data);
+    }
+    fetchData();
+  }, [movieId]);
+
+  if (!movie) return null;
+
+  return (
+    <section>
+      <img
+        className="modal_poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="img"
+      />
+    </section>
+  );
 };
 
 export default DetailPage;
